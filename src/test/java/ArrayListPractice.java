e99e1ec7d31faf55f9208bc7f369cbe28c776a2a
Amit Kapoor <kapooramit81@gmail.com>
2023-02-26 01:20:31 -0500

    Hashmap functions added

--- src/test/java/ArrayListPractice.java
+++ src/test/java/ArrayListPractice.java
@@ -1,48 +1,60 @@
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import java.util.*;
 
 public class ArrayListPractice
 {
-
       private static ArrayList<String> arrayListObj1;
       private static ArrayList<String> arrayListObj2;
-      ArrayListPractice()
+      private static ListIterator<String> lItr;
+
+      @BeforeTest
+      public void CreateArrayList()
       {
             arrayListObj1= new ArrayList<String>();
             arrayListObj2= new ArrayList<String>();
+            arrayListObj1.add("Hi");
+            arrayListObj1.add(1,"I'm ArrayList");
       }
 
       @Test
-      public static void addElementToArrayList()
+      public static void PrintArrayListSize()
       {
-          System.out.println(arrayListObj1.size());
-          arrayListObj1.add("Amit");
-          arrayListObj1.add(1,"Kapoor");
           System.out.println(arrayListObj1.size());
 
       }
     @Test
     public static void addListToAnotherArrayList()
     {
-
-        arrayListObj2.add("Ramit");
-        arrayListObj1.add("Ram");
-        arrayListObj1.add(1,"Kapoor");
+        arrayListObj2.add("RamiKapor");
         arrayListObj2.addAll(arrayListObj1);
         System.out.println(arrayListObj2);
-
     }
-
     @Test
     public static void displayingArrayListEle()
     {
 
-        addElementToArrayList();
         for(String str:arrayListObj1)
         {
             System.out.println(str);
         }
 
     }
+    @Test
+    public static void listIteratorTraverse()
+    {
 
+         lItr= arrayListObj1.listIterator();
+         while(lItr.hasNext())
+         {
+             System.out.println(lItr.next());
+             lItr.add("Pandey");
+         }
+
+        while(lItr.hasPrevious())
+        {
+            System.out.println(lItr.previous());
+        }
+
+    }
 }
--- src/test/java/HashMapPractice.java
+++ src/test/java/HashMapPractice.java
@@ -1,10 +1,56 @@
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.HashMap;
 
 public class HashMapPractice
 {
-  @Test
-   public static void test()
-   {
-   }
+    private static HashMap<String,String> map;
+
+    @BeforeTest
+    public static void createMap()
+    {
+
+        map=new HashMap<String,String>();
+        map.put("FName","Amit");
+        map.put("LName","Kapoor");
+    }
+
+    @Test
+    public static void getMapElementBasedOnKey()
+    {
+        System.out.println(map.get("FName"));
+        System.out.println(map.get("LName"));
+
+    }
+
+    @Test
+    public static void validateKeyValueInMap()
+    {
+        System.out.println(map.containsKey("FName"));
+        System.out.println(map.containsValue("Kapoor"));
+
+    }
+
+    @Test
+    public static void getSizeOfMap()
+    {
+        System.out.println(map.size());
+
+    }
+
+    @Test
+    public static void removeAKeyFromMap()
+    {
+        System.out.println(map.remove("FName"));
+        System.out.println(map.size());
+    }
+
+    @Test
+    public static void replaceAValueFromMap()
+    {
+         map.replace("FName","Ram");
+         System.out.println(map.get("FName"));
+    }
 
 }
